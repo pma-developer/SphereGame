b932153ecb3b989ed1aac023c2f772ac5639dcc8
pma-developer <demolitionok@gmail.com>
2024-04-11 12:08:14 +0300

    add FPS counter

---
+++ Assets/Scripts/FPSCounter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEngine;
+using TMPro;
+
+namespace SphereGame
+{
+    [RequireComponent(typeof(TMP_Text))]
+    public class FPSCounter : MonoBehaviour
+    {
+        private TMP_Text fpsText;
+
+        private void Awake()
+        {
+            fpsText = GetComponent<TMP_Text>();
+        }
+
+        private void Update()
+        {
+            var fps = (int)(1f / Time.unscaledDeltaTime);
+            fpsText.text = $"FPS : {fps:0.}";
+        }
+    }
+
+}
